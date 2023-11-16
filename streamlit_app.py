555740140104ec2d644e71313c7bbfc8bdb5cd46
selvidurai2k <150997700+selvidurai2k@users.noreply.github.com>
2023-11-16 12:11:31 +0530

    Update streamlit_app.py
    Added moms favorite diner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner')
+st.title('My Moms New Healthy Diner')
 
-st.header("Breakfast Menu")
-st.text("Omega 3 & Blueberry Oatmeal")
-st.text("Kale, Spinach & Rocket Smoothie ")
-st.text("Hard=Boiled Free-Range Egg")
+st.header("Breakfast Favorites")
+st.text("🥣 Omega 3 & Blueberry Oatmeal")
+st.text("🥗 Kale, Spinach & Rocket Smoothie ")
+st.text("🐔 Hard=Boiled Free-Range Egg")
+st.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
