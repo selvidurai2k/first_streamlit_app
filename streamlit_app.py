f2dfdff9adba8922c90c046f1bff43effea9b994
selvidurai2k <150997700+selvidurai2k@users.noreply.github.com>
2023-11-16 12:20:21 +0530

    Update streamlit_app.py
    added meaningfull index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 st.title('My Moms New Healthy Diner')
 
 st.header("Breakfast Favorites")
