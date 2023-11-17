ad7df9423d04be5ed307e6cba86f59069b5556f0
selvidurai2k <150997700+selvidurai2k@users.noreply.github.com>
2023-11-17 13:16:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ fruit_choice = st.text_input('What fruit would you like information about?','Kiw
 st.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+st.stop()
 import snowflake.connector
 
 
