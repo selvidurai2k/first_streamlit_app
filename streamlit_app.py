f965eb0645ea0b459ac485736880cf3002a0a99f
selvidurai2k <150997700+selvidurai2k@users.noreply.github.com>
2023-11-17 10:46:50 +0530

    Update streamlit_app.py
    user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 # show the output in a table  
